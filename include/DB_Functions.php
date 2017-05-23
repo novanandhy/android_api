69e43cf585bf19f0bec6cc507d3ba771ad0134f7
NOVAN\Novan <novanandhytrianto@gmail.com>
2017-05-24 00:45:48 +0700

    start project

---
+++ include/DB_Functions.php
@@ -0,0 +1,132 @@
+<?php
+
+/**
+ * @author Ravi Tamada
+ * @link http://www.androidhive.info/2012/01/android-login-and-registration-with-php-mysql-and-sqlite/ Complete tutorial
+ */
+
+class DB_Functions {
+
+    private $conn;
+
+    // constructor
+    function __construct() {
+        require_once 'DB_Connect.php';
+        // connecting to database
+        $db = new Db_Connect();
+        $this->conn = $db->connect();
+    }
+
+    // destructor
+    function __destruct() {
+        
+    }
+
+    /**
+     * Storing new user
+     * returns user details
+     */
+    public function storeUser($name, $previllage, $username, $password) {
+        $uuid = uniqid('', true);
+        $hash = $this->hashSSHA($password);
+        $encrypted_password = $hash["encrypted"]; // encrypted password
+        $salt = $hash["salt"]; // salt
+
+        $stmt = $this->conn->prepare("INSERT INTO user_tes(unique_id, name, username, encrypted_password, salt, previllage, created_at) VALUES(?, ?, ?, ?, ?, ?, NOW())");
+        $stmt->bind_param("ssssss", $uuid, $name, $username, $encrypted_password, $salt, $previllage);
+        $result = $stmt->execute();
+        $stmt->close();
+
+        // check for successful store
+        if ($result) {
+            $stmt = $this->conn->prepare("SELECT * FROM user_tes WHERE  username = ?");
+            $stmt->bind_param("s", $username);
+            $stmt->execute();
+            $user = $stmt->get_result()->fetch_assoc();
+            $stmt->close();
+
+            return $user;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Get user by username and password
+     */
+    public function getUserByUsernameAndPassword($username, $password) {
+
+        $stmt = $this->conn->prepare("SELECT * FROM user_tes WHERE username = ?");
+
+        $stmt->bind_param("s", $username);
+
+        if ($stmt->execute()) {
+            $user = $stmt->get_result()->fetch_assoc();
+            $stmt->close();
+
+            // verifying user password
+            $salt = $user['salt'];
+            $encrypted_password = $user['encrypted_password'];
+            $hash = $this->checkhashSSHA($salt, $password);
+            // check for password equality
+            if ($encrypted_password == $hash) {
+                // user authentication details are correct
+                return $user;
+            }
+        } else {
+            return NULL;
+        }
+    }
+
+    /**
+     * Check user is existed or not
+     */
+    public function isUserExisted($username) {
+        $stmt = $this->conn->prepare("SELECT username from user_tes WHERE username = ?");
+
+        $stmt->bind_param("s", $username);
+
+        $stmt->execute();
+
+        $stmt->store_result();
+
+        if ($stmt->num_rows > 0) {
+            // user existed 
+            $stmt->close();
+            return true;
+        } else {
+            // user not existed
+            $stmt->close();
+            return false;
+        }
+    }
+
+    /**
+     * Encrypting password
+     * @param password
+     * returns salt and encrypted password
+     */
+    public function hashSSHA($password) {
+
+        $salt = sha1(rand());
+        $salt = substr($salt, 0, 10);
+        $encrypted = base64_encode(sha1($password . $salt, true) . $salt);
+        $hash = array("salt" => $salt, "encrypted" => $encrypted);
+        return $hash;
+    }
+
+    /**
+     * Decrypting password
+     * @param salt, password
+     * returns hash string
+     */
+    public function checkhashSSHA($salt, $password) {
+
+        $hash = base64_encode(sha1($password . $salt, true) . $salt);
+
+        return $hash;
+    }
+
+}
+
+?>
